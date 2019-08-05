483512675a937cb78ddfff0443ab74f39bffdd0d
Codegym DN <codegymdanang@gmail.com>
2019-08-05 22:09:08 +0700

    Initial commit

---
+++ src/main/java/com/example/demo/configure/ApplicationConstants.java
@@ -0,0 +1,15 @@
+package com.example.demo.configure;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+public class ApplicationConstants {
+
+  public static final  List<Locale> LOCALES = Arrays.asList(new Locale("en"),
+            new Locale("es"),
+            new Locale("fr"),
+            new Locale("es", "MX"),
+            new Locale("zh"),
+            new Locale("ja"));
+}
---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+
+    @Autowired
+    MessageSource messageSource;
+
+    @GetMapping("/home")
+    public String greeting() {
+        /**
+         *   @LocaleContextHolder.getLocale()
+         *  Return the Locale associated with the given user context,if any, or the system default Locale otherwise.
+         *  This is effectively a replacement for Locale.getDefault(), able to optionally respect a user-level Locale setting.
+         */
+        String[] params= new String[]{"Ikhiloya", "today"};
+        return messageSource.getMessage("good.morning", params, LocaleContextHolder.getLocale());
+    }
+
+}
